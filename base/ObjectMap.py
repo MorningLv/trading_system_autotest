02f326523c7d6de7da1871d3f571b8ce7f00b79a
morning <632247309@qq.com>
2023-08-19 14:05:09 +0800

    封装等待页面完全加载

--- base/ObjectMap.py
+++ base/ObjectMap.py
@@ -1,14 +1,14 @@
 # @Time: 2023/8/19 11:37
 # @Author: LCC
 import time
-from selenium.common.exceptions import ElementNotVisibleException
+from selenium.common.exceptions import ElementNotVisibleException, WebDriverException
 
 
 class ObjectMap:
     def element_get(self, driver, locate_type, locator_expression, timeout=10, must_visible=False):
-        start_ms = time.time() * 1000
+        start_ms = time.time() * 1000  # 获取当前时间，毫秒
         stop_ms = start_ms + (timeout * 1000)
-        for i in range(int(timeout * 10)):
+        for i in range(int(timeout * 10)):  # 循环100次，每次time.sleep(0.1)即0.1秒，一共10秒
             try:
                 element = driver.find_element(by=locate_type, value=locator_expression)
                 if not must_visible:
@@ -24,3 +24,24 @@ class ObjectMap:
                     break
             time.sleep(0.1)
         raise ElementNotVisibleException("元素定位失败，定位方式：" + locate_type + ",定位表达式：" + locator_expression)
+
+    def wait_for_ready_state_complete(self, driver, timeout=30):
+        start_ms = time.time() * 1000
+        stop_ms = start_ms + (timeout * 1000)
+        for i in range(int(timeout * 10)):
+            try:
+                # 获取页面加载状态
+                ready_state = driver.execute_script("return document.readyState")
+            except WebDriverException:
+                # 如果driver出错，执行js失败会直接跳过
+                time.sleep(0.03)
+                return True
+            if ready_state == "complete":
+                time.sleep(0.01)
+                return True
+            else:
+                now_ms = time.time() * 1000
+                if now_ms > stop_ms:
+                    break
+                time.sleep(0.1)
+        raise Exception("页面云荣盛在%s秒后仍然没有完全加载完成" % timeout)
