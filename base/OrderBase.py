6f929b12c37de0497236a4fd99a36ad8d484bfda
morning <632247309@qq.com>
2023-08-23 20:35:05 +0800

    在我的订单列表页对订单的元素定位和操作

--- base/OrderBase.py
+++ base/OrderBase.py
@@ -4,3 +4,9 @@
 class OrderBase:
     def order_tab(self, tab_name):
         return "//div[@role='tab' and text()='" + tab_name + "']"
+
+    def order_operation(self, product_title, operation):
+        return "//div[text()='" + product_title + "']/ancestor::tr//span[text()='" + operation + "']/parent::button"
+
+    def order_operation_confirm(self):
+        return "//div[@class='el-dialog__wrapper' and contains(@style,'index')]//span[text()='确定']/parent::button"
--- pages/OrderPage.py
+++ pages/OrderPage.py
@@ -5,8 +5,20 @@ from selenium.webdriver.common.by import By
 from base.OrderBase import OrderBase
 from base.ObjectMap import ObjectMap
 
+from logs.log import log
+
 
 class OrderPage(OrderBase, ObjectMap):
     def click_order_tab(self, driver, tab_name):
         tab_xpath = self.order_tab(tab_name)
         return self.element_click(driver, By.XPATH, tab_xpath)
+
+    def click_order_operation(self, driver, product_title, operation):
+        log.info("订单标题为：" + product_title + ",点击订单的操作：" + operation)
+        button_xpath = self.order_operation(product_title, operation)
+        return self.element_click(driver, By.XPATH, button_xpath)
+
+    def click_order_operation_confirm(self, driver):
+        log.info("点击订单操作按钮后的弹框的确认按钮")
+        button_xpath = self.order_operation_confirm()
+        return self.element_click(driver, By.XPATH, button_xpath)
